2efa0847be9cd262fde7e4df579abe5a1712984e
ssaeed85 <saadsaeed85@gmail.com>
2022-06-01 09:11:21 -0500

    Working stable version of app

--- mars_anomaly_detection_app.py
+++ mars_anomaly_detection_app.py
@@ -11,7 +11,7 @@ import shutil
 import numpy as np
 np.random.seed(42)
 
-def drawBoundingRectangle(coordsTup , ImgObj, color='#ff0000', width=5, alpha=1.0):
+def drawBoundingRectangle(coordsTup , imgObj, color='#ff0000', width=5, alpha=1.0):
     """
     Creates a box to highligt a location on the image object
 
@@ -29,7 +29,7 @@ def drawBoundingRectangle(coordsTup , ImgObj, color='#ff0000', width=5, alpha=1.
     color_with_opacity = color + hex(int(alpha*255))[-2:]
     
     # Draw a rectangle
-    draw = ImageDraw.Draw(ImgObj,'RGBA')
+    draw = ImageDraw.Draw(imgObj,'RGBA')
     
     p1_coord = coordsTup[0:2]
     p2_coord = coordsTup[2:4]
@@ -39,17 +39,17 @@ def drawBoundingRectangle(coordsTup , ImgObj, color='#ff0000', width=5, alpha=1.
 def createNewPotentialBboxes(imgObj):
     img_width,  img_height = imgObj.size
     overlap = 0.4
-    stride = 277*(1-overlap)
 
     df_list = []
     
-#     (277,277*1.25,277*1.5)
-    for winsize in [(277*0.5) , (277), (277*1.25)]:
-        winsize = round(winsize)
+
+    for winsize in [ (277*0.75) , (277), (277*1.5)]:
+        winsize = round(winsize)        
+        
         if (winsize > img_width) or (winsize > img_height):
-            break
-            
-            
+            break            
+        
+        stride = round(winsize*(1-overlap))   
         #if winsize odd, increase by 1 pixel. Just easier
         if (winsize % 2) != 0:
             winsize+=1
@@ -88,15 +88,17 @@ def getPotentialBboxes(imgObj,forceCreateNew = False):
     return df_bboxPotentials
 
 def sliceAndDice(listOfImages):
-    slicePath = f"{'/'.join(im.filename.split('/')[:-1])}/_slices_{im.filename.split('/')[-1]}/"
-    slicePath = slicePath.replace('.','_')
+
+    fname = imgFile.name
+    slicePath = f"./mass_maps/_slices_img/"
+    st.write(slicePath)
 
     if (os.path.exists(slicePath) and os.path.isdir(slicePath)):
         shutil.rmtree(slicePath)
     os.makedirs(slicePath)
 
-    for count, img in enumerate(list_imagesToClassify):
-        img.save(''.join([slicePath,str(count),'_',(im.filename.split('/')[-1])]))
+    for count, img in enumerate(listOfImages):
+        img.save(''.join([slicePath,str(count),'_',(fname.split('/')[-1])]))
 
 def getClassFromPred(pred,threshold):
     if max(pred)>threshold :
@@ -104,22 +106,21 @@ def getClassFromPred(pred,threshold):
     return -1
 
 
-def loadImageAndInit():
-    # st.image(img_path)
-    # im = Image.open(imgFile)
-    # img_width,  img_height = im.size
-    # df_bbox = getPotentialBboxes(im,forceCreateNew = True)
-    # list_imagesToClassify = [im.crop(df_bbox.iloc[rowNum]['bbox_bounds']) 
-    #                     for rowNum in range(0,df_bbox.shape[0])]
-    disableButton = False
 
-options={}
+
+
+
+# ------------------------------------- #
+# ----------------MAIN----------------- #
+# ------------------------------------- #
+
+
 colorMap={}
 getPredictionsAndVisualize = False
 img_path=None
 disableButton=True
 im_wAnnotations=None
-model = tf.keras.models.load_model('final_finalModel__jk__butReally_FinalModel.h5py')
+model = tf.keras.models.load_model('tf_TransferLearning_8class_VGG16.hfpy')
 class_names = {'bright dune': 0,
  'crater': 1,
  'dark dune': 2,
@@ -133,63 +134,67 @@ class_names = {'bright dune': 0,
 with st.sidebar:
     st.title('Select colors for features:')    
     colorMap[class_names['bright dune']] = st.color_picker('Bright Dune', '#0796FF')
-    colorMap[class_names['crater']] = st.color_picker('Crater', '#D626CF')
-    colorMap[class_names['dark dune']] = st.color_picker('Dark Dune', '#8941F5')
-    colorMap[class_names['impact ejecta']] = st.color_picker('Impact Ejecta', '#46760A')
-    colorMap[class_names['slope streak']] = st.color_picker('Slope Streak', '#4C1634')
-    colorMap[class_names['spider']] = st.color_picker('Spider', '#C75280')
-    colorMap[class_names['swiss cheese']] = st.color_picker('Swiss Cheese', '#481363')
+    colorMap[class_names['crater']] = st.color_picker('Crater', '#FF00F4')
+    colorMap[class_names['dark dune']] = st.color_picker('Dark Dune', '#FF9700')
+    colorMap[class_names['impact ejecta']] = st.color_picker('Impact Ejecta', '#8DFF00')
     colorMap[class_names['other']] = st.color_picker('Other', '#F277FD')
+    colorMap[class_names['slope streak']] = st.color_picker('Slope Streak', '#FF008B')
+    colorMap[class_names['spider']] = st.color_picker('Spider', '#00475C')
+    colorMap[class_names['swiss cheese']] = st.color_picker('Swiss Cheese', '#000000')
 
 
 
 st.title("Mars Anomaly Detection")
 
-imgFile = st.file_uploader("Choose an image file", type="jpg",
-on_change=loadImageAndInit()
-)
+
+# Image loader
+# imgFile = st.file_uploader("Choose an image file", type="jpg")
+imgFile = st.file_uploader("Choose an image file", type="jpg")
 
 
 
-# if im_wAnnotations:
-#     st.image(im_wAnnotations)
-
-# if imgFile.on_change():
-#     st.image(imgFile)
-#     im = Image.open(imgFile)
-#     img_width,  img_height = im.size
-#     df_bbox = getPotentialBboxes(im,forceCreateNew = True)
-#     list_imagesToClassify = [im.crop(df_bbox.iloc[rowNum]['bbox_bounds']) 
-#                         for rowNum in range(0,df_bbox.shape[0])]
-#     disableButton = False
+if imgFile:
+    im = Image.open(imgFile)
+    st.image(im,caption='Welcome To Maahz')
+    disableButton = False
+    img_width,  img_height = im.size
+    df_bbox = getPotentialBboxes(im,forceCreateNew = True)
+    list_imagesToClassify = [im.crop(df_bbox.iloc[rowNum]['bbox_bounds']) 
+                        for rowNum in range(0,df_bbox.shape[0])]
 
 
-
-
-
+# Controls. Disabled until image is loaded
+# Multiselect toolbar
 options = st.multiselect(
      'Which features would you like to identify',
      ['Crater', 'Bright Dunes', 'Dark Dune', 'Impact Ejecta',
      'Slope Streak','Spider','Swiss Cheese','Other'],
-     ['Crater'],
+     ['Crater', 'Bright Dunes', 'Dark Dune', 'Impact Ejecta',
+     'Slope Streak','Spider','Swiss Cheese'],
      disabled=disableButton
      )
-
 options = [str.lower(x) for x in options]
 classes_to_visualize = [v for k,v in class_names.items() if k in options]
 
-thresh = st.slider('Confidence Threshold',value=0.9,min_value = 0.01, max_value= 1.0,disabled=disableButton)
+
+# Confidence threshold slider
+thresh = st.slider('Confidence Threshold',
+value=0.80,
+min_value = 0.01, 
+max_value= 1.0,
+disabled=disableButton)
+
+
 
 if st.button('Get Predictions', disabled=disableButton):
     sliceAndDice(list_imagesToClassify)
 
     predict_datagen = ImageDataGenerator(rescale=1./255)
 
-    # print(slicePath)
     predict_generator = predict_datagen.flow_from_directory(
         'mass_maps/',
         target_size = (227,227),
-        batch_size=8,
+        batch_size=100,
         color_mode='rgb',
         class_mode=None
     )
@@ -202,12 +207,11 @@ if st.button('Get Predictions', disabled=disableButton):
     mask_img = Image.new("RGBA", (img_width, img_height), (0, 0, 0, 0))
     df_validBbox = df_bbox[df_bbox['predClass'].isin(classes_to_visualize)][['bbox_bounds','predClass']]
 
-    for idx in range(0, df_validBbox.shape[0]):
-        
+    for idx in range(0, df_validBbox.shape[0]):        
         drawBoundingRectangle(df_validBbox.iloc[idx]['bbox_bounds'],
                             mask_img,
                             color=colorMap[df_validBbox.iloc[idx]['predClass']]
-                                            )
+                            )
 
     im_wAnnotations = im.convert('RGB')
 
